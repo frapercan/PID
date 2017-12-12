7d909ba3c0c936f9d00a2284992528a0f9a37224
angfra95 <angelco256@gmail.com>
2017-12-13 00:47:06 +0100

    Implementación de funcion de celdas
    Implementación de una función que crea un grid segun unas dimensiones y un tamaño de celdas.
    
    Implementación de una función que crea un grid segun una imagen como array pasada por parametros.

--- saliencyDetection.py
+++ saliencyDetection.py
@@ -1,6 +1,9 @@
 from skimage import io, color, filters
 import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
+import itertools as it
+from matplotlib import pyplot as plt
+from PIL import Image
 
 rgb = io.imread("sea.jpg")
 
@@ -30,12 +33,48 @@ lista_saliency_map = [rgb1,rgb2,rgb3,lab1,lab2,lab3]
 
 
 
-def generar_grid(tam,tam_celda):
-    res = []
-    fila = 
-    for i in range(tam):
-        if(i % tam_celda == 0):
-            fila 
-            
-        
+def generar_grid(tam_x,tam_y,tam_celda):
+    #Inicialización grid
+    grid = []
+    #Tipo1: Tipo de fila que va alternando entre blanco y negro.
+    #Tipo2: Tipo de fila que es toda blanca.
+    #Iterador del tipo de Fila 1
+    def tipo1():
+        #n: valor que va incrementandose hasta alcanzar el tamaño en x de la imagen
+        #m: valor que se va actualizando que informa si se ha traspasado el tamaño de una celda (en x) en cada iteración
+        n = 0
+        m = 0
+        while n < tam_x:
+            if(m == 0):
+                yield [255,255,255]
+                m = tam_celda
+            else:
+                yield [0,0,0]
+                m -=1
+            n+=1
+    #Creación de los arrays genericos del tipo1 y tipo2 a traves de dos listas y pasandoles iteradores
+    tipo1 = np.array(list(tipo1()),dtype='uint8')        
+    tipo2 = np.array(list(it.repeat([255,255,255],tam_x)),dtype='uint8')
+    #Inicialización de un valor para controlar la separación entre celdas.
+    m = 0
+    #Unión de las filas teniendo en cuenta la separación entre celdas.
+    for _ in range(tam_y):
+        if(m == 0):
+            grid.append(tipo2)
+            m = tam_celda
+        else:
+            grid.append(tipo1)
+            m-=1
+    return np.array(grid,dtype='uint8')
     
+#Genera un grid segun una imagen como array de numpy
+def generar_grid_segun_array(imagen,tam_celda):
+    shape = np.shape(imagen)
+    return generar_grid(shape[1],shape[0],tam_celda)
+    
+#Comandos de prueba
+#io.imshow(generar_grid(10,10,2))
+#io.imshow(generar_grid(10,10,3))
+
+
+#io.imsave('celda.png',generar_grid_segun_array(rgb,10))
