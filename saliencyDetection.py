4c5b9cb98deeceef2900342962ea794acecbbc10
angfra95 <angelco256@gmail.com>
2017-12-13 21:21:12 +0100

    Limpieza parcial de codigo y generacion de imagenes de interseccion
    Limpieza y estructuracion de codigo. Generacion de imagenes con las intersecciones.

--- saliencyDetection.py
+++ saliencyDetection.py
@@ -28,11 +28,8 @@ hsv3 = filters.gaussian(hsv,gaussian_kernels[2])
 #No sabemos si los filtros gaussianos se aplican bien, se muestran verde al pasar de lab y hsv a rgb y mostrarlo
 lista_saliency_map = [rgb1,rgb2,rgb3,lab1,lab2,lab3]
 
-
-
-
-
-def generar_grid(tam_x,tam_y,tam_celda):
+# Genera una lista solo con los puntos de intersección del grid
+def generar_puntos_interseccion_grid(tam_x,tam_y,tam_celda):
     
     #dato -> (pos_x,pos_y)
     def iterador_grid():
@@ -54,9 +51,9 @@ def generar_grid(tam_x,tam_y,tam_celda):
             m=0
             a=0
     return list(iterador_grid())
-            
-''''
-def generar_grid(tam_x,tam_y,tam_celda):
+
+# Genera el array de la imagen del grid.       
+def generar_grid_entero(tam_x,tam_y,tam_celda):
     #Inicialización grid
     grid = []
     #Tipo1: Tipo de fila que va alternando entre blanco y negro.
@@ -90,27 +87,42 @@ def generar_grid(tam_x,tam_y,tam_celda):
             m-=1
     return np.array(grid,dtype='uint8')
 
-'''
 
-#Genera un grid segun una imagen como array de numpy
-def generar_grid_segun_array(array_imagen,tam_celda):
+
+#Genera un grid segun una imagen como array de numpy. Tiene dos opciones, o bien solo los pixeles de la intersección del grid (I) o bien todo el grid (otro)
+def generar_grid_a_traves_imagen_y_opcion(array_imagen,tam_celda,opcion=0):
     shape = np.shape(array_imagen)
-    return generar_grid(shape[1],shape[0],tam_celda)
+    if(opcion == 0):
+        return generar_puntos_interseccion_grid(shape[1],shape[0],tam_celda)
+    else:
+        return generar_grid_entero(shape[1],shape[0],tam_celda)
+
+#Genera la misma imagen pasada por parametros con un grid solapado.
+def generar_imagen_con_grid(nombre_fichero,array_imagen,tam_celda):
+   pass #TODO
+    
+    
 
 
-def interseccion_grid_imagen_segun_array(array_imagen,tam_celda):
-    grid = generar_grid_segun_array(array_imagen,tam_celda)
+#Genera una imagen solo con los puntos de intersección del grid con la imagen pasada por parametros.
+def generar_imagen_interseccion(nombre_fichero,array_imagen,tam_celda):
     
-    resultado = []
-    for (x,y) in grid:
-        resultado.append((x,y,array_imagen[x][y]))
-    return resultado
+    def puntos_interseccion(array_imagen,tam_celda):
+        grid = generar_grid_a_traves_imagen_y_opcion(array_imagen,tam_celda)
+        resultado = []
+        for (y,x) in grid:
+            resultado.append((y,x,array_imagen[y][x]))
+        return resultado
     
-    #return [(x,y,pixel) for (y,fila) in enumerate(array_imagen) for (x,pixel) in enumerate(fila)  if (x,y) in grid]
+    shape = np.shape(array_imagen)
+    interseccion = puntos_interseccion(array_imagen,tam_celda)
+    resultado = [[[127,127,127] for x in range(shape[1])] for y in range(shape[0])]
+    for (y,x,dato) in interseccion:
+        resultado[y][x] = dato
+    io.imsave(nombre_fichero,np.array(resultado,dtype='uint8'))
     
 #Comandos de prueba
-#io.imshow(generar_grid(10,10,2))
-#io.imshow(generar_grid(10,10,3))
+#generar_imagen_interseccion('imagen_interseccion.png',rgb,1)
+#generar_imagen_interseccion('imagen_interseccion2.png',rgb,10)
 
-
-#io.imsave('celda.png',generar_grid_segun_array(rgb,10))
+#io.imsave('celda.png',generar_grid_a_traves_imagen_y_opcion(rgb,10,1))
