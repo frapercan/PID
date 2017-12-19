7ec0bfcc2ee93784154e59085c43687a5a28ebe4
Santiago Tarrío <tarriosantiago@gmail.com>
2017-12-19 12:16:20 +0100

    Add files via upload

--- pid/mean_shift.py
+++ pid/mean_shift.py
@@ -4,23 +4,26 @@ import mean_shift_utils as ms_utils
 
 MIN_DISTANCE = 0.000001
 
-
 class MeanShift(object):
     def __init__(self, kernel=ms_utils.gaussian_kernel):
         if kernel == 'multivariate_gaussian':
             kernel = ms_utils.multivariate_gaussian_kernel
+        elif kernel == 'epanechnikov_kernel':
+            kernel = ms_utils.epanechnikov_kernel
         self.kernel = kernel
 
     def cluster(self, points, kernel_bandwidth, iteration_callback=None):
         if(iteration_callback):
             iteration_callback(points, 0)
         shift_points = np.array(points)
-        max_min_dist = 1
+        max_min_dist = 10000000
+        dist=0
+        dist_after=1
         iteration_number = 0
 
         still_shifting = [True] * points.shape[0]
         while max_min_dist > MIN_DISTANCE:
-            # print max_min_dist
+            print(max_min_dist)
             max_min_dist = 0
             iteration_number += 1
             for i in range(0, len(shift_points)):
@@ -29,7 +32,7 @@ class MeanShift(object):
                 p_new = shift_points[i]
                 p_new_start = p_new
                 p_new = self._shift_point(p_new, points, kernel_bandwidth)
-                dist = ms_utils.euclidean_dist(p_new, p_new_start)
+                dist = 0 if dist==(ms_utils.euclidean_dist(p_new[:2], p_new_start[:2])+ms_utils.euclidean_dist(p_new[2:5], p_new_start[2:5])) else ms_utils.euclidean_dist(p_new[:2], p_new_start[:2])+ms_utils.euclidean_dist(p_new[2:5], p_new_start[2:5])
                 if dist > max_min_dist:
                     max_min_dist = dist
                 if dist < MIN_DISTANCE:
@@ -51,6 +54,8 @@ class MeanShift(object):
         # denominator
         denominator = sum(point_weights)
         shifted_point = np.multiply(tiled_weights.transpose(), points).sum(axis=0) / denominator
+        #print(point-points)
+        #print(shifted_point==point)
         return shifted_point
 
         # ***************************************************************************
--- pid/mean_shift_utils.py
+++ pid/mean_shift_utils.py
@@ -11,13 +11,13 @@ def euclidean_dist(pointA, pointB):
     return math.sqrt(total)
 
 
-def gaussian_kernel(distance, bandwidth):
+def gaussian_kernel(distance, bandwidth, e=1):
     euclidean_distance = np.sqrt(((distance)**2).sum(axis=1))
     val = (1/(bandwidth*math.sqrt(2*math.pi))) * np.exp(-0.5*((euclidean_distance / bandwidth))**2)
     return val
 
 
-def multivariate_gaussian_kernel(distances, bandwidths):
+def multivariate_gaussian_kernel(distances, bandwidths, e=1):
 
     # Number of dimensions of the multivariate gaussian
     dim = len(bandwidths)
@@ -30,3 +30,20 @@ def multivariate_gaussian_kernel(distances, bandwidths):
     val = (1 / np.power((2 * math.pi), (dim/2)) * np.power(np.linalg.det(cov), 0.5)) * np.exp(exponent)
 
     return val
+
+def epanechnikov_kernel(distance,bw,e=1):
+    distance_s = distance[:,:2]/bw[0]
+    distance_r = distance[:,2:5]/bw[1]
+
+    ek_s = epanechnikov_profile(np.power(np.array([np.linalg.norm(distance_s[i]) for i in range(len(distance_s)) ]),2))
+    ek_r = epanechnikov_profile(np.power(np.array([np.linalg.norm(distance_r[i]) for i in range(len(distance_r)) ]),2))
+
+    #print(np.array([np.linalg.norm(distance_s[i]) for i in range(len(distance_s)) ]))
+    #print(np.array([np.linalg.norm(distance_r[i]) for i in range(len(distance_r)) ]))
+
+    val = (e/(bw[0]**2)*(bw[1]**2))*ek_s*ek_r
+
+    return val
+
+def epanechnikov_profile(values):
+    return np.array([1-x if np.linalg.norm(x)<=1 else 0 for x in values])
